8cd48505c2ad21718eb407d7bc82b81c605276de
caz002 <115192246+caz002@users.noreply.github.com>
2022-11-15 14:38:08 -0800

    fsf

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,49 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+class AChecker implements StringChecker{
+  public boolean checkString(String s){
+    for(i = 0; i < s.length() - 1; i++){
+      if(s.substring(i, i+1).equals("a") || s.substring(i, i+1).equals("A")){
+        return true;
+      }
+    }
+  }
+}
 public class TestListExamples {
   // Write your grading tests here!
+
+
+  //Tests of static List<String> filter(List<String> s, StringChecker sc)
+  @Test
+  public void testFilterReturn(){
+    ArrayList<String> list1 = new ArrayList<String>();
+    list1.add("A");
+    list1.add("B");
+    list1.add("C");
+    list1.add("D");
+    list1.add("E");
+    list1.add("F");
+
+    ArrayList<String> expected = new ArrayList<String>();
+    expected.add("A");
+    assertEquals(expected, list1);
+  }
+  //Tests of static List<String> merge(List<String> list1, List<String> list2)
+  @Test
+  public void testMergeReturn(){
+    ArrayList<String> list1 = new ArrayList<String>();
+    ArrayList<String> list2 = new ArrayList<String>();
+    list1.add("B");
+    list1.add("F");
+    list1.add("H");
+
+    list2.add("C");
+    list2.add("D");
+    list2.add("K");
+
+    ArrayList<String> expected = new ArrayList<String>();
+    expected.add("B"); expected.add("C"); expected.add("D"); expected.add("D"); expected.add("F"); expected.add("H");expected.add("K");
+    assertEquals(expected, merge(list1, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -4,4 +4,18 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+if[[-e "ListExamples.java" ]]
+then
+    echo "ListExamples.java was found! +1 points"
+else
+    echo "ListExamples.java is missing +0 points"
+fi
+logout
+cp GradeServer.java Server.java TestListExamples.java student-submission
+javac *.java
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+set +e
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples.java
+echo $?
 
